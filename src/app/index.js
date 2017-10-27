e878e4df1a91922812f6a83ec51a1fdcdd00edaa
SuperGerb <stephaniecarter@live.ca>
2017-10-27 13:22:33 -0400

    Adds the form for creating a classified ad. Puts a button on the homepage that links to this form. Saves the input values to state in the ClassifiedAdForm component

--- src/app/index.js
+++ src/app/index.js
@@ -2,14 +2,13 @@ import $ from 'jquery';
 import Bootstrap from 'bootstrap';
 import React from 'react';
 //import ReactDOM from 'react-dom';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import App from '../components/App';
 import { BrowserRouter } from 'react-router-dom';
 
 
 
-$(document).ready(function () { 
+$(document).ready(function () {
     render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 });
 //ReactDOM.render(<App />, document.getElementById('root'));
-
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
 import ClassifiedAdsList from './ClassifiedAdsList';
+import CreateClassifiedAdButton from './CreateClassifiedAdButton';
 
 const Home = (props) => (
-    <ClassifiedAdsList />
+    <div>
+        <ClassifiedAdsList />
+        <CreateClassifiedAdButton />
+    </div>
 )
 
 export default Home;
