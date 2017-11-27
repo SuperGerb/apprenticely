028770481031ee3ba3deb4093e477223fcb74f23
SuperGerb <stephaniecarter@live.ca>
2017-11-27 11:26:55 -0500

    Adds a CTA in the hero area to browse the latest postings, and when clicked it scrolls to that area of the page. Displays a confirmation message when redirected from the classified ad form page, that fades out after a few seconds. Adds a call to action to sign up, at the bottom of the page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,77 @@
 import React, { Component } from 'react';
 import ClassifiedAdsList from './ClassifiedAdsList';
 import CreateClassifiedAdButton from './CreateClassifiedAdButton';
+import SignupCTA from './SignupCTA';
 import PrfUserProfileList from './user/list/PrfUserProfileList';
 import PrfHttpClient from 'profilic-client';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      profileList:[]
+    this.state = {
+      profileList: []
     };
     this.prfClient = new PrfHttpClient();
   }
 
-  componentDidMount =()=>{
+  componentDidMount = () => {
     this.getLatestProfiles();
+    this.fadeOutMessageBox();
   }
 
-  getLatestProfiles =()=> {
+  getLatestProfiles = () => {
     this.prfClient.getProfiles(this.populateList);
   }
 
   populateList = (responseData) => {
     console.log("**************************|", responseData);
 
-    if(!responseData) {
+    if (!responseData) {
       console.log("Error retrieving list of profiles!");
-      return;
+      return;  
     }
-    this.setState({ profileList:responseData.profileList });
+    this.setState({ profileList: responseData.profileList });
+  }
+ 
+  scrollToPostings = () => {
+    let distance = $(".appr-main").offset().top - 60;
+    $("html,body").animate({scrollTop: distance}, 600);
+  }
+
+  fadeOutMessageBox=()=>{
+    $(".confirmation-message-box").delay(2000).fadeOut();
   }
 
   render() {
+    //For displaying success message when redirecting from ClassifiedAdForm component: 
+    if (this.props.location.state !== undefined) {
+      var message = this.props.location.state.message;
+    }
+
     return (
       <div>
+        <CreateClassifiedAdButton />
         <section className="appr-bgimage short">
+          {message ? <div className="confirmation-message-box"><p>{message}</p></div> : null}
+          <div className="row appr-hero width-constrained">
+            <div className="appr-hero-box">
+              <h4 className="appr-hero-caption">Find collabs, gigs, equipment, constructive critiques and more.</h4>
+              {/* <p className="appr-hero-copy">Apprenticely gathers together the best postings relating to the arts. See what others are working on. Pick up a new hobby. Post samples of your latest project.</p>
+              The journey to mastery begins with practice */}
+            </div>
+          </div>
+          <p className="appr-main-cta" onClick={this.scrollToPostings}>browse the latest postings<i className="fa fa-angle-down" aria-hidden="true"></i></p>
         </section>
         <section className="appr-main">
           <div>
-              <ClassifiedAdsList />
-              <CreateClassifiedAdButton />
-              <h3>Our newest Members</h3>
-              <PrfUserProfileList profileList={this.state.profileList} 
-                orientation="vertical"
-                notifyOnChanges={this.getLatestProfiles} />
+            <ClassifiedAdsList /> 
+            <h3>Our newest Members</h3>
+            <PrfUserProfileList profileList={this.state.profileList}
+              orientation="vertical"
+              notifyOnChanges={this.getLatestProfiles} />
           </div>
         </section>
+        <SignupCTA />
       </div>
     );
   }
